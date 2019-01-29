3877cb78a1f11de92971f73275ce9585f26ca514
Maile Lucks <maile.lucks@gmail.com>
2019-01-29 08:52:45 -0800

    JS for Customization Form

--- httpdocs/customization_landing_pages/x-customization-form.js
+++ httpdocs/customization_landing_pages/x-customization-form.js
@@ -1,30 +1,59 @@
-(function () {
-	'use strict';
+$(document).ready( function(){
 
-	var contactForm = document.querySelector('[data-hook="customization-contact-form"]');
+	(function () {
+		'use strict';
+		
 
-	if (contactForm) {
-		var contactFormAction = contactForm.getAttribute('data-action');
-		var contactFormButton = document.querySelector('[data-hook="contact-form__submit"]');
+		var contactForm = document.querySelector('[data-hook="customization-contact-form"]');
 
-		contactForm.classList.remove('u-hidden');
+		if (contactForm) {
+			var contactFormAction = contactForm.getAttribute('data-action');
+			var contactFormButton = document.querySelector('[data-hook="customization-contact-form__submit"]');
 
-		/**
-		 * Unlock the form if the user is utilizing a touch interface.
-		 */
-		window.addEventListener('touchstart', function touchedForm() {
-			contactForm.setAttribute('action', contactFormAction);
-			contactFormButton.removeAttribute('disabled');
-			window.removeEventListener('touchstart', touchedForm, false);
-		}, false);
+			contactForm.classList.remove('u-hidden');
 
-		/**
-		 * Unlock the form if the user can hover over elements.
-		 */
-		window.addEventListener('mouseover', function mouseForm() {
-			contactForm.setAttribute('action', contactFormAction);
-			contactFormButton.removeAttribute('disabled');
-			window.removeEventListener('mouseover', mouseForm, false);
-		}, false);
-	}
-})();
+			/**
+			 * Unlock the form if the user is utilizing a touch interface.
+			 */
+			window.addEventListener('touchstart', function touchedForm() {
+				contactForm.setAttribute('action', contactFormAction);
+				contactFormButton.removeAttribute('disabled');
+				window.removeEventListener('touchstart', touchedForm, false);
+			}, false);
+
+			/**
+			 * Unlock the form if the user can hover over elements.
+			 */
+			window.addEventListener('mouseover', function mouseForm() {
+				contactForm.setAttribute('action', contactFormAction);
+				contactFormButton.removeAttribute('disabled');
+				window.removeEventListener('mouseover', mouseForm, false);
+			}, false);
+
+			$(contactForm).submit(function(e) {
+
+				e.preventDefault(); // avoid to execute the actual submit of the form.
+
+			    var url = $(contactForm).attr('action');
+
+			    $.ajax({
+		            type: "POST",
+		            url: url,
+		            data:$(contactForm).serialize(), // serializes the form's elements.
+		            success: function(data)
+		            {
+		                $('.x-messages--success').removeClass('u-hidden');
+		            },
+		            fail: function(data) {
+		           		$('.x-messages--error').removeClass('u-hidden');
+		            }
+			    });
+
+			    
+			});
+
+		}
+
+	})();
+
+});
